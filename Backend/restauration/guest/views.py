7d0e82997d74c2d63dae17ab2516ac884bf38df0
adrianwzorek <167366@student.uwm.edu.pl>
2024-11-21 14:45:21 +0100

    Home View for any

--- Backend/restauration/guest/views.py
+++ Backend/restauration/guest/views.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import ListAPIView
+from rest_framework import permissions
 from api.models import Menu
 from api.serializers import MenuSerializer
 
 class Home(ListAPIView):
-    queryset = Menu.objects.all()
+    queryset = Menu.objects.filter(active = True)
     serializer_class = MenuSerializer
+    permission_classes = [permissions.AllowAny]
