cfca5be8e3ac020bc281ff124e0e71bcb7599a30
adrianwzorek <167366@student.uwm.edu.pl>
2024-12-06 13:15:09 +0100

    Change view of manage Bill

--- Backend/restauration/guest/urls.py
+++ Backend/restauration/guest/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GetBill, CreateNewGuest,Dishes, Drinks, HomeFirst ,Home, DrinkDetails, DishDetails, GetAllergen
+from .views import ManageBill, CreateNewGuest,Dishes, Drinks, HomeFirst ,Home, DrinkDetails, DishDetails, GetAllergen
 urlpatterns = [
     path('home/', Home.as_view(), name='home_table'),
     path('home/<int:pk>/', HomeFirst.as_view(), name='home_table'),
@@ -8,7 +8,6 @@ urlpatterns = [
     path('home/dish/allergen/<int:pk>/', GetAllergen.as_view(), name='allergens'),
     path('home/drink/', Drinks.as_view()),
     path('home/drink/<int:pk>/',DrinkDetails.as_view(), name='drink'),
-    path('bill/<int:pk>/', GetBill.as_view(),name='bill'),
+    path('bill/<int:pk>/', ManageBill.as_view(),name='bill'),
     path('login/<int:pk>/',CreateNewGuest.as_view() , name='new_user'),
-    path('bill/<int:id_bill>',GetBill.as_view(), name='bill_details'),
 ]
